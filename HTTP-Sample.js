379775c1d8acf85a8be737258df8e8a459065910
Balaji-Kalikivai <kalikivai999@gmail.com>
2025-02-17 11:52:15 +0530

    Upto HTTP

--- HTTP-Sample.js
+++ HTTP-Sample.js
@@ -3,9 +3,18 @@
 const http = require('http')
 
 const myServer = http.createServer((req, res) => {
-    res.write("Welcome to the first NodeJS Server");
+    res.write("Welcome to the NodeJS Server");
     res.end();
 }
 )
+
+
+const { addNumber, subtractNumber, multiplyNumber, divideNumber } = require('./localDemoModule');
+
+console.log(addNumber(10, 5));
+console.log(subtractNumber(10, 5));
+console.log(multiplyNumber(10, 5));
+console.log(divideNumber(10, 5));
+
 
 myServer.listen(5500)
---
+++ localDemoImportModule.js
@@ -0,0 +1,7 @@
+
+const { addNumber, subtractNumber, multiplyNumber, divideNumber } = require('./localDemoModule');
+
+console.log(addNumber(10, 5));
+console.log(subtractNumber(10, 5));
+console.log(multiplyNumber(10, 5));
+console.log(divideNumber(10, 5));
